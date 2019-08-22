2764b331cf9b91e6379a43aca604ad4c01a5c40c
Joshua Bohde <bohdecoded@gmail.com>
2019-08-22 19:30:51 -0400

    refactor: update models eslint

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,29 +1,29 @@
-module.exports = function (sequelize, DataTypes) {
-    var doctor = sequelize.define("doctor", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        patient_id: {
-            type: DataTypes.STRING,
-        },
-        doctor_name: {
-            type: DataTypes.STRING,
-        },
-        user_name: {
-            type: DataTypes.STRING,
-        },
-        password: {
-            type: DataTypes.STRING,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-        }
-    });
+module.exports = function(sequelize, DataTypes) {
+  var doctor = sequelize.define('doctor', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    patient_id: {
+      type: DataTypes.STRING,
+    },
+    doctor_name: {
+      type: DataTypes.STRING,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  });
 
-    return doctor;
+  return doctor;
 };
--- models/saveRecipes.js
+++ models/saveRecipes.js
@@ -1,35 +1,36 @@
-module.exports = function (sequelize, DataTypes) {
-    var savedRecipes = sequelize.define("savedRecipes", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        patient_id: {
-            type: DataTypes.INTEGER,
-        },
-        recipe_name: {
-            type: DataTypes.STRING,
-        },
-        recipe_img: {
-            type: DataTypes.STRING,
-        },
-        recipe: {
-            type: DataTypes.STRING,
-        },
-        recipe_uri: {
-            type: DataTypes.STRING,
-        },
-        favorite: {
-            type: DataTypes.BOOLEAN, defaultValue: false
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-        }
-    });
+module.exports = function(sequelize, DataTypes) {
+  var savedRecipes = sequelize.define('savedRecipes', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    patient_id: {
+      type: DataTypes.INTEGER,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+    },
+    recipe_img: {
+      type: DataTypes.STRING,
+    },
+    recipe: {
+      type: DataTypes.STRING,
+    },
+    recipe_uri: {
+      type: DataTypes.STRING,
+    },
+    favorite: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  });
 
-    return savedRecipes;
+  return savedRecipes;
 };
