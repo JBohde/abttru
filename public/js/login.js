7a0deb5b901c94bb777a09a946b8a9797208059d
Joshua Bohde <bohdecoded@gmail.com>
2020-07-18 09:35:24 -0400

    refactor: javascript files

---
+++ public/js/login.js
@@ -0,0 +1,68 @@
+$(document).ready(() => {
+  $('#login').val($('#login option:first').val());
+  $('#user-input').val('');
+  let userQ;
+
+  function createSlider(response) {
+    $.each(response.hits, (i, hit) => {
+      const { recipe } = hit;
+      const itemDiv = $('<div>')
+        .attr({
+          class: 'carousel-item',
+          'data-id': i,
+        });
+
+      const image = $('<img>')
+        .attr({
+          id: recipe.uri,
+          class: 'img-fluid recipe-img',
+          src: recipe.image,
+          alt: recipe.label,
+          'data-id': i,
+        });
+
+      const caption = $('<a>')
+        .attr({
+          href: recipe.url,
+          target: '_blank',
+          role: 'button',
+          class: 'plotly-anchor',
+          position: 'absolute',
+        })
+        .text(recipe.label);
+
+      itemDiv.append(caption, image);
+      $('.carousel-item').first().addClass('active');
+      $('.carousel-inner').append(itemDiv);
+      $('.carousel').carousel('pause');
+      $('.recipe-area').show();
+    });
+  }
+
+  $('.search').on('click', event => {
+    $('.doctor-form').hide();
+    $('.please-login').hide();
+    event.preventDefault();
+    userQ = $('#user-input').val().trim();
+    // risk_factor = $("#risk-factor").val().toLowerCase().trim();
+    const dietFactor = $('#diet-factor').val().toLowerCase().trim();
+    const dietRestriction = $('#diet-restriction').val().toLowerCase().trim();
+    $.ajax({
+      url: `https://api.edamam.com/search?q=${userQ}&app_id=76461587&app_key=b829a690de0595f2fa5b7cb02db4cd99&from=0&to=5&diet=${dietFactor}&health=${dietRestriction}`,
+      method: 'GET',
+    }).done(response => {
+      createSlider(response);
+    });
+  });
+
+  $('.login').on('change', () => {
+    const loginSelector = $('#login').val().toLowerCase().trim();
+    if (loginSelector === 'patient') {
+      $('.patient-form').show();
+      $('.doctor-form').hide();
+    } else if (loginSelector === 'dietitian') {
+      $('.patient-form').hide();
+      $('.doctor-form').show();
+    }
+  });
+});
--- public/js/plotly.js
+++ public/js/plotly.js
@@ -1,217 +1,216 @@
-module.exports = {
-  createPlots(response, index) {
-    const plotDigest = response.hits[index].recipe.digest;
-    const plotYield = response.hits[index].recipe.yield;
+// eslint-disable-next-line no-unused-vars
+function createPlots(response, index) {
+  const plotDigest = response.hits[index].recipe.digest;
+  const plotYield = response.hits[index].recipe.yield;
 
-    const firstPlot = {
-      values: [],
-      labels: [],
-    };
-    const secondPlot = {
-      values: [],
-      labels: [],
-    };
-    const thirdPlot = {
-      values: [],
-      labels: [],
-    };
-    const fourthPlot = {
-      values: [],
-      labels: [],
-    };
+  const firstPlot = {
+    values: [],
+    labels: [],
+  };
+  const secondPlot = {
+    values: [],
+    labels: [],
+  };
+  const thirdPlot = {
+    values: [],
+    labels: [],
+  };
+  const fourthPlot = {
+    values: [],
+    labels: [],
+  };
 
-    const ultimateColors = [
-      ['#2E5894', '#8FD400', '#9C2542'],
-      ['#0095B7', '#2D383A', '#1AB385', '#A50B5E'],
-      [
-        '#ABAD48',
-        '#469496',
-        '#436CB9',
-        '#469A84',
-        '#353839',
-        '#2D5DA1',
-        '#64609A',
-      ],
-      [
-        '#E97451',
-        '#FC80A5',
-        '#C62D42',
-        '#C9A0DC',
-        '#76D7EA',
-        '#FF8833',
-        '#29AB87',
-        '#AF593E',
-        '#01786F',
-        '#FFCBA4',
-        '#FCD667',
-        '#ED0A3F',
-        '#FBE870',
-      ],
-    ];
+  const ultimateColors = [
+    ['#2E5894', '#8FD400', '#9C2542'],
+    ['#0095B7', '#2D383A', '#1AB385', '#A50B5E'],
+    [
+      '#ABAD48',
+      '#469496',
+      '#436CB9',
+      '#469A84',
+      '#353839',
+      '#2D5DA1',
+      '#64609A',
+    ],
+    [
+      '#E97451',
+      '#FC80A5',
+      '#C62D42',
+      '#C9A0DC',
+      '#76D7EA',
+      '#FF8833',
+      '#29AB87',
+      '#AF593E',
+      '#01786F',
+      '#FFCBA4',
+      '#FCD667',
+      '#ED0A3F',
+      '#FBE870',
+    ],
+  ];
 
-    plotDigest.forEach((nutrient, i) => {
-      if (
-        nutrient.label === 'Fat'
+  plotDigest.forEach((nutrient, i) => {
+    if (
+      nutrient.label === 'Fat'
       || nutrient.label === 'Carbs'
       || nutrient.label === 'Protein'
-      ) {
-        firstPlot.values.push(nutrient.total / plotYield);
-        firstPlot.labels.push(nutrient.label);
+    ) {
+      firstPlot.values.push(nutrient.total / plotYield);
+      firstPlot.labels.push(nutrient.label);
 
-        if (nutrient.label === 'Fat') {
-          nutrient.sub.forEach(fat => {
-            secondPlot.values.push(fat.total / plotYield);
-            secondPlot.labels.push(fat.label);
-          });
-        }
-      } else if ((i > 3) && (i < 11)) {
-        thirdPlot.values.push(nutrient.total / plotYield);
-        thirdPlot.labels.push(nutrient.label);
-      } else if ((i > 10) && (i < 24)) {
-        fourthPlot.values.push(nutrient.total / plotYield);
-        fourthPlot.labels.push(nutrient.label);
+      if (nutrient.label === 'Fat') {
+        nutrient.sub.forEach(fat => {
+          secondPlot.values.push(fat.total / plotYield);
+          secondPlot.labels.push(fat.label);
+        });
       }
-    });
+    } else if ((i > 3) && (i < 11)) {
+      thirdPlot.values.push(nutrient.total / plotYield);
+      thirdPlot.labels.push(nutrient.label);
+    } else if ((i > 10) && (i < 24)) {
+      fourthPlot.values.push(nutrient.total / plotYield);
+      fourthPlot.labels.push(nutrient.label);
+    }
+  });
 
-    // // process nutrition data
-    // const totalNutrients = response.hits[1].recipe.plotDigest;
+  // // process nutrition data
+  // const totalNutrients = response.hits[1].recipe.plotDigest;
 
-    // // calculate totalNutrient per serving for our recipe
-    // for (const key in totalNutrients) {
-    //   if (totalNutrients.hasOwnProperty(key)) {
-    //     const val = totalNutrients[key];
-    //     const perServing = val.quantity / 8;
-    //   }
-    // }
+  // // calculate totalNutrient per serving for our recipe
+  // for (const key in totalNutrients) {
+  //   if (totalNutrients.hasOwnProperty(key)) {
+  //     const val = totalNutrients[key];
+  //     const perServing = val.quantity / 8;
+  //   }
+  // }
 
-    // populate the website with beautiful plots
-    const dataOne = [
-      {
-        values: firstPlot.values,
-        labels: firstPlot.labels,
-        name: 'Macronutrients',
-        hoverinfo: 'label+percent+name',
-        hole: 0.6,
-        type: 'pie',
-        marker: {
-          colors: ultimateColors[0],
-        },
+  // populate the website with beautiful plots
+  const dataOne = [
+    {
+      values: firstPlot.values,
+      labels: firstPlot.labels,
+      name: 'Macronutrients',
+      hoverinfo: 'label+percent+name',
+      hole: 0.6,
+      type: 'pie',
+      marker: {
+        colors: ultimateColors[0],
       },
-    ];
+    },
+  ];
 
-    const dataTwo = [
+  const dataTwo = [
+    {
+      values: secondPlot.values,
+      labels: secondPlot.labels,
+      text: 'Fats',
+      textposition: 'inside',
+      name: 'Lipids',
+      hoverinfo: 'label+percent+name',
+      hole: 0.6,
+      type: 'pie',
+      marker: {
+        colors: ultimateColors[1],
+      },
+    },
+  ];
+
+  const dataThree = [
+    {
+      values: thirdPlot.values,
+      labels: thirdPlot.labels,
+      name: 'Minerals',
+      textposition: 'inside',
+      hoverinfo: 'label+percent+name',
+      hole: 0.6,
+      type: 'pie',
+    },
+  ];
+
+  const dataFour = [
+    {
+      values: fourthPlot.values,
+      labels: fourthPlot.labels,
+      text: 'Vitamins',
+      textposition: 'inside',
+      name: 'Vitamins',
+      hoverinfo: 'label+percent+name',
+      hole: 0.6,
+      type: 'pie',
+    },
+  ];
+
+  const layoutOne = {
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: 'transparent',
+    annotations: [
       {
-        values: secondPlot.values,
-        labels: secondPlot.labels,
+        font: { size: 18, color: '#749541' },
+        showarrow: false,
+        text: 'Macros',
+        x: 0.5,
+        y: 0.5,
+      },
+    ],
+    showlegend: false,
+    width: 350,
+    height: 400,
+  };
+
+  const layoutTwo = {
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: 'transparent',
+    annotations: [
+      {
+        font: { size: 18, color: '#749541' },
+        showarrow: false,
         text: 'Fats',
-        textposition: 'inside',
-        name: 'Lipids',
-        hoverinfo: 'label+percent+name',
-        hole: 0.6,
-        type: 'pie',
-        marker: {
-          colors: ultimateColors[1],
-        },
+        x: 0.5,
+        y: 0.5,
       },
-    ];
+    ],
+    showlegend: false,
+    width: 350,
+    height: 400,
+  };
 
-    const dataThree = [
+  const layoutThree = {
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: 'transparent',
+    annotations: [
       {
-        values: thirdPlot.values,
-        labels: thirdPlot.labels,
-        name: 'Minerals',
-        textposition: 'inside',
-        hoverinfo: 'label+percent+name',
-        hole: 0.6,
-        type: 'pie',
+        font: { size: 18, color: '#749541' },
+        showarrow: false,
+        text: 'Minerals',
+
+        x: 0.5,
+        y: 0.5,
       },
-    ];
+    ],
+    showlegend: false,
+    width: 350,
+    height: 400,
+  };
 
-    const dataFour = [
+  const layoutFour = {
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: 'transparent',
+    annotations: [
       {
-        values: fourthPlot.values,
-        labels: fourthPlot.labels,
+        font: { size: 18, color: '#749541' },
+        showarrow: false,
         text: 'Vitamins',
-        textposition: 'inside',
-        name: 'Vitamins',
-        hoverinfo: 'label+percent+name',
-        hole: 0.6,
-        type: 'pie',
+        x: 0.5,
+        y: 0.5,
       },
-    ];
+    ],
+    showlegend: false,
+    width: 350,
+    height: 400,
+  };
 
-    const layoutOne = {
-      paper_bgcolor: 'transparent',
-      plot_bgcolor: 'transparent',
-      annotations: [
-        {
-          font: { size: 18, color: '#749541' },
-          showarrow: false,
-          text: 'Macros',
-          x: 0.5,
-          y: 0.5,
-        },
-      ],
-      showlegend: false,
-      width: 350,
-      height: 400,
-    };
-
-    const layoutTwo = {
-      paper_bgcolor: 'transparent',
-      plot_bgcolor: 'transparent',
-      annotations: [
-        {
-          font: { size: 18, color: '#749541' },
-          showarrow: false,
-          text: 'Fats',
-          x: 0.5,
-          y: 0.5,
-        },
-      ],
-      showlegend: false,
-      width: 350,
-      height: 400,
-    };
-
-    const layoutThree = {
-      paper_bgcolor: 'transparent',
-      plot_bgcolor: 'transparent',
-      annotations: [
-        {
-          font: { size: 18, color: '#749541' },
-          showarrow: false,
-          text: 'Minerals',
-
-          x: 0.5,
-          y: 0.5,
-        },
-      ],
-      showlegend: false,
-      width: 350,
-      height: 400,
-    };
-
-    const layoutFour = {
-      paper_bgcolor: 'transparent',
-      plot_bgcolor: 'transparent',
-      annotations: [
-        {
-          font: { size: 18, color: '#749541' },
-          showarrow: false,
-          text: 'Vitamins',
-          x: 0.5,
-          y: 0.5,
-        },
-      ],
-      showlegend: false,
-      width: 350,
-      height: 400,
-    };
-
-    Plotly.newPlot('macros-container', dataOne, layoutOne);
-    Plotly.newPlot('fats-container', dataTwo, layoutTwo);
-    Plotly.newPlot('minerals-container', dataThree, layoutThree);
-    Plotly.newPlot('vitamins-container', dataFour, layoutFour);
-  }
-};
+  Plotly.newPlot('macros-container', dataOne, layoutOne);
+  Plotly.newPlot('fats-container', dataTwo, layoutTwo);
+  Plotly.newPlot('minerals-container', dataThree, layoutThree);
+  Plotly.newPlot('vitamins-container', dataFour, layoutFour);
+}
